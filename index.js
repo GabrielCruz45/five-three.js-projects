6802092894144dc9e8610160b7e26aae74ce8042
Gabriel Cruz <gabriel.crzprz45@gmail.com>
2026-02-23 08:15:10 -0400

    second sketch with primitives; some jamming with lights

--- index.js
+++ index.js
@@ -79,10 +79,62 @@ torusWireMesh.scale.setScalar(1.0001);
 torusMesh.add(torusWireMesh);
 
 
+// torus knot!!
+const torusKnot_geo = new THREE.TorusKnotGeometry(0.1, 0.030, 100, 16);
+const torusKnot_mat = new THREE.MeshStandardMaterial({ 
+    color: 0xffffff,
+    flatShading: true,
+});
+
+const torusKnot_mesh = new THREE.Mesh(torusKnot_geo, torusKnot_mat);
+torusKnot_mesh.position.x = 0.4;
+scene.add(torusKnot_mesh);
+
+const torusKnot_wireMesh = new THREE.Mesh(torusKnot_geo, wireMat);
+torusKnot_wireMesh.scale.setScalar(1.0001);
+torusKnot_mesh.add(torusKnot_wireMesh);
+
+const torusKnot_mesh_two = new THREE.Mesh(torusKnot_geo, torusKnot_mat);
+torusKnot_mesh_two.position.x = -0.4;
+scene.add(torusKnot_mesh_two);
+
+const torusKnot_wireMesh_two = new THREE.Mesh(torusKnot_geo, wireMat);
+torusKnot_wireMesh_two.scale.setScalar(1.0001);
+torusKnot_mesh_two.add(torusKnot_wireMesh_two);
+
+
+
+
+
 
 //                                                                  5. Light!
+// const light = new THREE.AmbientLight( 0xff0000 ); // soft white light
+// scene.add( light );
+
+
 const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
 scene.add(hemiLight);
+
+// // White directional light at half intensity shining from the top.
+// const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+// scene.add( directionalLight );
+
+// const light = new THREE.PointLight( 0xff0000, 1, 100 );
+// light.position.set( 50, 50, 50 );
+// scene.add( light );
+
+// // white spotlight shining from the side, modulated by a texture
+// const spotLight = new THREE.SpotLight( 0xffffff );
+// spotLight.position.set( 100, 1000, 100 );
+// spotLight.map = new THREE.TextureLoader().load( url );
+// spotLight.castShadow = true;
+// spotLight.shadow.mapSize.width = 1024;
+// spotLight.shadow.mapSize.height = 1024;
+// spotLight.shadow.camera.near = 500;
+// spotLight.shadow.camera.far = 4000;
+// spotLight.shadow.camera.fov = 30;
+
+
 
 
 
@@ -92,6 +144,24 @@ function animate(t = 0) {
     torusMesh.rotation.x = t * 0.001;
     torusMesh.rotation.y = t * 0.001;
     torusMesh.rotation.z = t * 0.0001;
+
+    torusKnot_mesh.rotation.x = t * -0.001;
+    torusKnot_mesh.rotation.y = t * -0.001;
+    torusKnot_mesh.rotation.z = t * -0.0001;
+    
+    torusKnot_mesh_two.rotation.x = t * 0.001;
+    torusKnot_mesh_two.rotation.y = t * 0.001;
+    torusKnot_mesh_two.rotation.z = t * 0.0001;
+
+    torusKnot_mesh.position.x = Math.cos(t * 0.001);
+    torusKnot_mesh.position.y = Math.cos(t * 0.001);
+    torusKnot_mesh.position.z = Math.sin(t * 0.001) * 1.1;
+    
+    torusKnot_mesh_two.position.x = Math.sin(t * 0.0005);
+    torusKnot_mesh_two.position.y = Math.sin(t * 0.0005);
+    torusKnot_mesh_two.position.z = Math.sin(t * 0.001) * 0.7;
+    
+
     renderer.render(scene, camera);
     controls.update();
 };
